111d2aafab10245fa0d0a6ca8c6c55e61d6d911c
Ihor Podpriadok <podpryadok@gmail.com>
2019-08-08 16:12:40 +0300

    Create tests for Login page

--- tests/loginForm.spec.js
+++ tests/loginForm.spec.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const expect = require('chai').expect;
+const config = require('../lib/config');
 
 describe('Login page tests', () => {
     let browser;
@@ -7,15 +8,16 @@ describe('Login page tests', () => {
 
     before(async () => {
         browser = await puppeteer.launch({
-            headless: true,
-            slowMo: 0,
-            devtools: false,
-            timeout: 10000,
+            headless: config.isHeadLess,
+            slowMo: config.slowMo,
+            devtools: config.devtools,
+            timeout: config.launchTimeout,
         });
         page = await browser.newPage();
+        await page.setDefaultTimeout(config.waitingTimeout);
         await page.setViewport({
-            width: 800,
-            height: 600
+            width: config.viewportWeiht,
+            height: config.viewportHeight
         });
     });
 
@@ -25,7 +27,8 @@ describe('Login page tests', () => {
     });
 
     it('Open the page', async () => {
-        await page.goto("https://hub-staging.clockwise.software/login");
+        //await page.goto("https://hub-staging.clockwise.software/login");
+        await page.goto(config.baseURl);
         await page.waitForSelector("#email");
 
         const url = await page.url();
@@ -34,6 +37,10 @@ describe('Login page tests', () => {
         expect(url).to.contain("https://hub-staging.clockwise.software/login");
         expect(title).to.contains("Clockwise Hub");
     });
+
+    // it('Select the "Remember me" chackbox', async () => {
+    //     await page.click(".checkbox");
+    // });
 
     it('Valid log in', async () => {
         await page.type("#email", "admin@gmail.com");
@@ -52,15 +59,16 @@ describe('Login page invalid tests', () => {
 
     before(async () => {
         browser = await puppeteer.launch({
-            headless: true,
-            slowMo: 0,
-            devtools: false,
-            timeout: 10000,
+            headless: config.isHeadLess,
+            slowMo: config.slowMo,
+            devtools: config.devtools,
+            timeout: config.launchTimeout,
         });
         page = await browser.newPage();
+        await page.setDefaultTimeout(config.waitingTimeout);
         await page.setViewport({
-            width: 800,
-            height: 600
+            width: config.viewportWeiht,
+            height: config.viewportHeight
         });
     });
 
@@ -70,8 +78,8 @@ describe('Login page invalid tests', () => {
     });
 
     it('Showing exceptions when inputs were empty', async () => {
-        await page.goto("https://hub-staging.clockwise.software/login");
-
+        //await page.goto("https://hub-staging.clockwise.software/login");
+        await page.goto(config.baseURl);
         await page.waitForSelector(".btn.btn-primary");
         await page.click(".btn.btn-primary");
         await page.waitForSelector(".help-block");
@@ -82,5 +90,55 @@ describe('Login page invalid tests', () => {
         await page.type("#password", "admin1");
         await page.click(".btn.btn-primary");
         await page.waitForSelector(".help-block");
+    });
+});
+
+describe('Forgot password page', () => {
+    let browser;
+    let page;
+
+    before(async () => {
+        browser = await puppeteer.launch({
+            headless: config.isHeadLess,
+            slowMo: config.slowMo,
+            devtools: config.devtools,
+            timeout: config.launchTimeout,
+        });
+        page = await browser.newPage();
+        await page.setDefaultTimeout(config.waitingTimeout);
+        await page.setViewport({
+            width: config.viewportWeiht,
+            height: config.viewportHeight
+        });
+    });
+
+    after(async () => {
+        //await page.waitFor(3000);
+        await browser.close();
+    });
+
+    it('Click on the "Forgot password" button',async () => {
+        await page.goto(config.baseURl);
+        await page.waitForSelector("#email");
+
+        await page.click(".btn.btn-link");
+        await page.waitForSelector("#email");
+
+        const url = page.url();
+        await expect(url).to.contain("https://hub-staging.clockwise.software/password/reset");
+    });
+
+    it('Send new password', async () => {
+        await page.type("#email","admin@gmail.com");
+        await page.click(".btn.btn-primary");
+        await page.waitForSelector(".alert.alert-success");
+    });
+
+    it('Back to login page', async () => {
+        await page.click(".nav.navbar-nav.navbar-right");
+        await page.waitForSelector("#password");
+
+        const url = page.url();
+        await expect(url).to.contain("https://hub-staging.clockwise.software/login");
     });
 });
