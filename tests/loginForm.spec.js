85e40e148045fe7a6d66472abd806fd1b8e2d860
Ihor Podpriadok <podpryadok@gmail.com>
2019-08-21 16:05:17 +0300

    Create new functions and add their in the tests

--- tests/loginForm.spec.js
+++ tests/loginForm.spec.js
@@ -1,115 +1,113 @@
-const puppeteer = require('puppeteer');
-const expect = require('chai').expect;
+const puppeteer = require("puppeteer");
+const expect = require("chai").expect;
 
-const config = require('../lib/config');
-const click = require('../lib/helpers').click;
-const typeText = require('../lib/helpers').typeText;
-const loadUrl = require('../lib/helpers').loadUrl;
-const waitForText = require('../lib/helpers').waitForText;
-const shouldExist = require('../lib/helpers').shouldExist;
+const config = require("../lib/config");
+const funkctions = require("../lib/helpers");
+const utils = require("../lib/utils");
 
-const loginPageLocator = require('../page-objects/login.page');
-const homePageLocator = require('../page-objects/home.page');
+const loginPageLocator = require("../page-objects/login.page");
+const homePageLocator = require("../page-objects/home.page");
 
 let browser;
 let page;
 
 before(async () => {
-    browser = await puppeteer.launch({
-        headless: config.isHeadLess,
-        slowMo: config.slowMo,
-        devtools: config.devtools,
-        timeout: config.launchTimeout,
-    });
-    page = await browser.newPage();
-    await page.setDefaultTimeout(config.waitingTimeout);
-    await page.setViewport({
-        width: config.viewportWeiht,
-        height: config.viewportHeight
-    });
+  browser = await puppeteer.launch({
+    headless: config.isHeadLess,
+    slowMo: config.slowMo,
+    devtools: config.devtools,
+    timeout: config.launchTimeout
+  });
+  page = await browser.newPage();
+  await page.setDefaultTimeout(config.waitingTimeout);
+  await page.setViewport({
+    width: config.viewportWeiht,
+    height: config.viewportHeight
+  });
 });
 
 after(async () => {
-    await browser.close();
+  await browser.close();
 });
 
-describe('Login page invalid tests', () => {
+describe("Login page invalid tests", () => {
+  it("Showing exceptions when inputs were empty", async () => {
+    await funkctions.loadUrl(page, config.baseURl);
 
-    it('Showing exceptions when inputs were empty', async () => {
-        await loadUrl(page, config.baseURl);
+    await funkctions.waitForText(page, "body", "Login");
 
-        await waitForText(page, 'body', 'Login');
+    await funkctions.click(page, loginPageLocator.loginBtn);
+    await funkctions.shouldExist(page, loginPageLocator.helpText);
+  });
 
-        await click(page, loginPageLocator.loginBtn);
-        await shouldExist(page, loginPageLocator.helpText);
-    });
-
-    it('Invalid log in ', async () => {
-        await typeText(page, loginPageLocator.emailInput, "admin@gmail.com");
-        await typeText(page, loginPageLocator.passwordInput, "admin1");
-        await click(page, loginPageLocator.loginBtn);
-        await shouldExist(page, loginPageLocator.helpText);
-    });
+  it("Invalid log in ", async () => {
+    await funkctions.typeText(page, loginPageLocator.emailInput, utils.generateEmail());
+    await funkctions.typeText(page, loginPageLocator.passwordInput, utils.generateID(5));
+    await funkctions.click(page, loginPageLocator.loginBtn);
+    await funkctions.shouldExist(page, loginPageLocator.helpText);
+  });
 });
 
-describe('Forgot password page', () => {
+describe("Forgot password page", () => {
+  it('Click on the "Forgot password" button', async () => {
+    await funkctions.loadUrl(page, config.baseURl);
+    await funkctions.shouldExist(page, loginPageLocator.emailInput);
 
-    it('Click on the "Forgot password" button',async () => {
-        await loadUrl(page, config.baseURl);
-        await shouldExist(page, loginPageLocator.emailInput);
+    await funkctions.click(page, loginPageLocator.rememberMeBtn);
+    await funkctions.shouldExist(page, loginPageLocator.emailInput);
 
-        await click(page, loginPageLocator.rememberMeBtn);
-        await shouldExist(page, loginPageLocator.emailInput);
+    const url = page.url();
+    await expect(url).to.contain(
+      "https://hub-staging.clockwise.software/password/reset"
+    );
+  });
 
-        const url = page.url();
-        await expect(url).to.contain("https://hub-staging.clockwise.software/password/reset");
-    });
+  it("Send new password", async () => {
+    await funkctions.typeText(page, loginPageLocator.emailInput, "admin@gmail.com");
+    await funkctions.click(page, loginPageLocator.loginBtn);
+    await funkctions.shouldExist(page, loginPageLocator.alertSucces);
+  });
 
-    it('Send new password', async () => {
-        await typeText(page, loginPageLocator.emailInput,"admin@gmail.com");
-        await click(page, loginPageLocator.loginBtn);
-        await shouldExist(page, loginPageLocator.alertSucces);
-    });
+  it("Back to login page", async () => {
+    await funkctions.click(page, loginPageLocator.loginBackBtn);
+    await funkctions.shouldExist(page, loginPageLocator.passwordInput);
 
-    it('Back to login page', async () => {
-        await click(page, loginPageLocator.loginBackBtn);
-        await shouldExist(page, loginPageLocator.passwordInput);
-
-        const url = page.url();
-        await expect(url).to.contain("https://hub-staging.clockwise.software/login");
-    });
+    const url = page.url();
+    await expect(url).to.contain(
+      "https://hub-staging.clockwise.software/login"
+    );
+  });
 });
 
-describe('Login page tests', () => {
+describe("Login page tests", () => {
+  it("Open the page", async () => {
+    await funkctions.loadUrl(page, config.baseURl);
+    await funkctions.shouldExist(page, loginPageLocator.emailInput);
 
-    it('Open the page', async () => {
-        await loadUrl(page, config.baseURl);
-        await shouldExist(page, loginPageLocator.emailInput);
+    const url = await page.url();
+    const title = await page.title();
 
-        const url = await page.url();
-        const title = await page.title();
+    expect(url).to.contain("https://hub-staging.clockwise.software/login");
+    expect(title).to.contains("Clockwise Hub");
+  });
 
-        expect(url).to.contain("https://hub-staging.clockwise.software/login");
-        expect(title).to.contains("Clockwise Hub");
+  it('Select the "Remember me" chackbox', async () => {
+    await funkctions.click(page, loginPageLocator.checkbox);
+
+    const onCheckbox = await page.evaluate(() => {
+      return document.querySelector('input[type="checkbox"]').checked;
     });
 
-    it('Select the "Remember me" chackbox', async () => {
-        await click(page, loginPageLocator.checkbox);
+    expect(onCheckbox).to.equal(true);
+  });
 
-        const onCheckbox = await page.evaluate(() => {
-            return document.querySelector('input[type="checkbox"]').checked;
-        });
+  it("Valid log in", async () => {
+    await funkctions.typeText(page, loginPageLocator.emailInput, "admin@gmail.com");
+    await funkctions.typeText(page, loginPageLocator.passwordInput, "admin");
+    await funkctions.click(page, loginPageLocator.loginBtn);
+    await funkctions.shouldExist(page, homePageLocator.nearestEventsBlock);
 
-        expect(onCheckbox).to.equal(true);
-    });
-
-    it('Valid log in', async () => {
-        await typeText(page, loginPageLocator.emailInput, "admin@gmail.com");
-        await typeText(page, loginPageLocator.passwordInput, "admin");
-        await click(page, loginPageLocator.loginBtn);
-        await shouldExist(page, homePageLocator.nearestEventsBlock);
-
-        const url = await page.url();
-        expect(url).to.contain("https://hub-staging.clockwise.software/home");
-    });
+    const url = await page.url();
+    expect(url).to.contain("https://hub-staging.clockwise.software/home");
+  });
 });
--- tests/rate.spec.js
+++ tests/rate.spec.js
@@ -1,116 +1,115 @@
-const puppeteer = require('puppeteer');
-const expect = require('chai').expect;
+const puppeteer = require("puppeteer");
+const expect = require("chai").expect;
 
-const config = require('../lib/config');
-const click = require('../lib/helpers').click;
-const typeText = require('../lib/helpers').typeText;
-const loadUrl = require('../lib/helpers').loadUrl;
-const shouldExist = require('../lib/helpers').shouldExist;
+const config = require("../lib/config");
+const funkctions = require("../lib/helpers");
 
-const loginPage = require('../page-objects/login.page');
-const homePage = require('../page-objects/home.page');
-const ratePage = require('../page-objects/rate.page');
+const loginPage = require("../page-objects/login.page");
+const homePage = require("../page-objects/home.page");
+const ratePage = require("../page-objects/rate.page");
 
 let browser;
 let page;
 
 before(async () => {
-    browser = await puppeteer.launch({
-        headless: config.isHeadLess,
-        slowMo: config.slowMo,
-        devtools: config.devtools,
-        timeout: config.launchTimeout,
-    });
-    page = await browser.newPage();
-    await page.setDefaultTimeout(config.waitingTimeout);
-    await page.setViewport({
-        width: config.viewportWeiht,
-        height: config.viewportHeight
-    });
+  browser = await puppeteer.launch({
+    headless: config.isHeadLess,
+    slowMo: config.slowMo,
+    devtools: config.devtools,
+    timeout: config.launchTimeout
+  });
+  page = await browser.newPage();
+  await page.setDefaultTimeout(config.waitingTimeout);
+  await page.setViewport({
+    width: config.viewportWeiht,
+    height: config.viewportHeight
+  });
 });
 
 after(async () => {
-    await browser.close();
+  await browser.close();
 });
 
-describe('Negative tests for Rate page', () => {
+describe("Negative tests for Rate page", () => {
+  it("log in to hub as an admin", async () => {
+    await funkctions.loadUrl(page, config.baseURl);
+    await funkctions.typeText(page, loginPage.emailInput, "admin@gmail.com");
+    await funkctions.typeText(page, loginPage.passwordInput, "admin");
+    await funkctions.click(page, loginPage.loginBtn);
+    await funkctions.shouldExist(page, homePage.nearestEventsBlock);
+  });
 
-    it('log in to hub as an admin', async () => {
-        await loadUrl(page, config.baseURl);
-        await typeText(page, loginPage.emailInput, "admin@gmail.com");
-        await typeText(page, loginPage.passwordInput, "admin");
-        await click(page, loginPage.loginBtn);
-        await shouldExist(page, homePage.nearestEventsBlock);
+  it("go to Rate page from Home page", async () => {
+    await funkctions.shouldExist(page, homePage.rateNavLink);
+    await funkctions.click(page, homePage.rateNavLink);
+    await funkctions.shouldExist(page, ratePage.pointsPannelHeading);
+    await funkctions.shouldExist(page, ratePage.usersTabContent);
+  });
+
+  it("check that logged in user doesnt see himself name in rate table", async () => {
+    const innerUsersText = await page.evaluate(() => {
+      return document.querySelector(".tab-content").innerText;
     });
+    expect(innerUsersText).to.not.contains("Admin");
+  });
 
-    it('go to Rate page from Home page', async () => {
-        await shouldExist(page, homePage.rateNavLink);
-        await click(page, homePage.rateNavLink);
-        await shouldExist(page, ratePage.pointsPannelHeading);
-        await shouldExist(page, ratePage.usersTabContent);
-    });
+  it("try to rate any user with empty reason input", async () => {
+    await funkctions.click(page, ratePage.upVoteBtn);
+    await page.waitFor(200); //timeout is needed because of voteBtn visibility
+    await funkctions.shouldExist(page, ratePage.modalForm);
 
-    it('check that logged in user doesnt see himself name in rate table', async () => {
-        const innerUsersText = await page.evaluate(() => {
-            return document.querySelector('.tab-content').innerText
-        });
-        expect(innerUsersText).to.not.contains('Admin');
-    });
+    await funkctions.click(page, ratePage.voteBtn);
+    await funkctions.shouldExist(page, ratePage.modalForm, true);
 
-    it('try to rate any user with empty reason input', async () => {
-        await click(page, ratePage.upVoteBtn);
-        await page.waitFor(200) //timeout is needed because of voteBtn visibility
-        await shouldExist(page, ratePage.modalForm);
+    await funkctions.click(page, ratePage.cancelBtn);
+    await funkctions.shouldExist(page, ratePage.modalForm, false);
+  });
 
-        await click(page, ratePage.voteBtn);
-        await shouldExist(page, ratePage.modalForm, true);
+  it("try to rate any user with reason shorter than 3 characters", async () => {
+    await page.waitFor(300);
+    await funkctions.click(page, ratePage.upVoteBtn);
+    await funkctions.shouldExist(page, ratePage.modalForm);
+    await page.waitFor(300);
 
-        await click(page, ratePage.cancelBtn);
-        await shouldExist(page, ratePage.modalForm, false);
-    });
+    await funkctions.click(page, ratePage.reasonTextarea);
+    await funkctions.typeText(page, ratePage.reasonTextarea, "12");
+    await funkctions.click(page, ratePage.voteBtn);
 
-    it('try to rate any user with reason shorter than 3 characters', async () => {
-        await click(page, ratePage.upVoteBtn);
-        await shouldExist(page, ratePage.modalForm);
+    await funkctions.shouldExist(page, ratePage.alertError);
 
-        await typeText(page, ratePage.reasonTextarea, '12');
-        await click(page, ratePage.voteBtn);
+    const element = await page.$(ratePage.alertError);
+    const text = await page.evaluate(element => element.textContent, element);
+    expect(text).to.contain("The reason must be at least 3 characters.");
+  });
+});
 
-        await shouldExist(page, ratePage.alertError);
+describe("Positive tests for Rate page", () => {
+  it("rate any user with valid reason", async () => {
+    await funkctions.click(page, ratePage.upVoteBtn);
+    await funkctions.shouldExist(page, ratePage.modalForm);
+    await page.waitFor(300);
 
-        const element = await page.$(ratePage.alertError);
-        const text = await page.evaluate(element => element.textContent, element);
-        expect(text).to.contain('The reason must be at least 3 characters.');
-    });
-})
+    await funkctions.click(page, ratePage.reasonTextarea);
+    await funkctions.typeText(page, ratePage.reasonTextarea, "test");
+    await funkctions.click(page, ratePage.voteBtn);
 
-describe('Positive tests for Rate page', () => {
+    await funkctions.shouldExist(page, ratePage.alertSuccess);
 
-    it('rate any user with valid reason', async () => {
-        await click(page, ratePage.upVoteBtn);
-        await shouldExist(page, ratePage.modalForm);
-        await page.waitFor(300);
+    const element = await page.$(ratePage.alertSuccess);
+    const text = await page.evaluate(element => element.textContent, element);
+    expect(text).to.contain("You voted successfully!");
+  });
 
-        await click(page, ratePage.reasonTextarea);
-        await typeText(page, ratePage.reasonTextarea, 'test');
-        await click(page, ratePage.voteBtn);
+  it("Unvote user", async () => {
+    //needed for clean up data - because tests are cycled
+    await funkctions.shouldExist(page, ratePage.downVoteBtn);
+    await funkctions.click(page, ratePage.downVoteBtn);
+    await funkctions.shouldExist(page, ratePage.alertSuccess);
 
-        await shouldExist(page, ratePage.alertSuccess);
-        
-        const element = await page.$(ratePage.alertSuccess);
-        const text = await page.evaluate(element => element.textContent, element);
-        expect(text).to.contain('You voted successfully!');
-    });
+    const element = await page.$(ratePage.alertSuccess);
+    const text = await page.evaluate(element => element.textContent, element);
+    expect(text).to.contain("You removed your vote successfully!");
 
-    it('Unvote user', async () => { //needed for clean up data - because tests are cycled
-        await shouldExist(page, ratePage.downVoteBtn);
-        await click(page, ratePage.downVoteBtn);
-        await shouldExist(page, ratePage.alertSuccess);
-        
-        const element = await page.$(ratePage.alertSuccess);
-        const text = await page.evaluate(element => element.textContent, element);
-        expect(text).to.contain('You removed your vote successfully!');
-
-        await shouldExist(page, ratePage.upVoteBtn);
-    });
+    await funkctions.shouldExist(page, ratePage.upVoteBtn);
+  });
 });
